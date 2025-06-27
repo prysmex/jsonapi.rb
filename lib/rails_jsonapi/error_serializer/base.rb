75df6ebdeed64128d15f9690544ee8959c31ffe5
Patricio de Villa <patricio.devilla@prysmex.com>
2025-06-26 20:17:35 -0600

    fix

--- lib/rails_jsonapi/error_serializer/base.rb
+++ lib/rails_jsonapi/error_serializer/base.rb
@@ -17,7 +17,7 @@ module RailsJSONAPI
       # define 'attribute's that support Object/Hash
       %i[id links status code title detail source meta].each do |name|
         attribute name do |object, _params|
-          object.try(name) || object.try(:fetch, name, nil)
+          object.try(name) || (object.is_a?(Hash) ? object[name] : nil)
         end
       end
 
